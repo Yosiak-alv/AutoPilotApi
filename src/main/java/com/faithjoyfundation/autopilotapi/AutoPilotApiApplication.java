be32d0dce04c912246af104dfceead66d3ef989b
Yosiak-alv <Josias_romero60@outlook.com>
2024-08-30 14:52:40 -0600

    initial commit

---
+++ src/main/java/com/faithjoyfundation/autopilotapi/AutoPilotApiApplication.java
@@ -0,0 +1,13 @@
+package com.faithjoyfundation.autopilotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPilotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoPilotApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/faithjoyfundation/autopilotapi/AutoPilotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faithjoyfundation.autopilotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPilotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
