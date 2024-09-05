82a6a4c52a9530fee746ad1c64ac1e50a9966952
Yosiak-alv <Josias_romero60@outlook.com>
2024-09-05 00:02:03 -0600

    custom enum validator

---
+++ src/main/java/com/faithjoyfundation/autopilotapi/v1/common/validation/IsEnum.java
@@ -0,0 +1,17 @@
+package com.faithjoyfundation.autopilotapi.v1.common.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Constraint(validatedBy = IsEnumValidator.class)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface IsEnum {
+    String message() default "invalid enum value";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends Enum<?>> enumClass();
+}
---
+++ src/main/java/com/faithjoyfundation/autopilotapi/v1/common/validation/IsEnumValidator.java
@@ -0,0 +1,27 @@
+package com.faithjoyfundation.autopilotapi.v1.common.validation;
+
+import jakarta.validation.ConstraintValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsEnumValidator implements ConstraintValidator<IsEnum, String> {
+
+    private List<String> values = new ArrayList<>();
+    @Override
+    public void initialize(IsEnum enumValue) {
+        values = new ArrayList<>();
+        Enum<?> [] enumConstants = enumValue.enumClass().getEnumConstants();
+        for (Enum<?> enumConstant : enumConstants) {
+            values.add(enumConstant.toString());
+        }
+    }
+
+    @Override
+    public boolean isValid(String value, jakarta.validation.ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        return values.contains(value);
+    }
+}
