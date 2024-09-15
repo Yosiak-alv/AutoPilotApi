ebac92e548b80f59a055091c0ce5f985e7e2697e
Yosiak-alv <Josias_romero60@outlook.com>
2024-09-14 19:53:48 -0600

    Update UpdateRepairStatusRequest.java

--- src/main/java/com/faithjoyfundation/autopilotapi/v1/dto/repair_managment/UpdateRepairStatusRequest.java
+++ src/main/java/com/faithjoyfundation/autopilotapi/v1/dto/repair_managment/UpdateRepairStatusRequest.java
@@ -1,7 +1,5 @@
 package com.faithjoyfundation.autopilotapi.v1.dto.repair_managment;
 
-import com.faithjoyfundation.autopilotapi.v1.common.validations.IsExists;
-import com.faithjoyfundation.autopilotapi.v1.persistence.repositories.RepairStatusRepository;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
@@ -10,6 +8,5 @@ import lombok.Data;
 public class UpdateRepairStatusRequest {
     @NotNull
     @Positive
-    //@IsExists(message = "repair status does not exist", repository = RepairStatusRepository.class)
     private Long repairStatusId;
 }
